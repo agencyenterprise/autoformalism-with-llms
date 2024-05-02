9d796ac81d785e56e9add17eda46d8c61f474345
Mike Vaiana <mikevaiana@gmail.com>
2024-05-02 15:42:43 -0500

    Update GPT-4 results notebook

--- src/autoformalism_with_llms/config.py
+++ src/autoformalism_with_llms/config.py
@@ -1,10 +1,10 @@
 from dotenv import find_dotenv
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
     OPENAI_API_KEY: str
-    model_config = {"env_file": find_dotenv(usecwd=True)}
+    model_config = SettingsConfigDict(env_file=find_dotenv(usecwd=True), extra="allow")
 
 
 config = Config()
